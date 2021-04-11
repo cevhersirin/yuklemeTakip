c07b05df8be6dfbe78a77ea388d229717bf4b862
cevher.sirin <cevher.sirin@enqura.com>
2021-04-11 15:17:17 +0300

    openImage on full screen

--- YuklemeTakip/Modules/ShipmentDetail/ShipmentDetailViewController.swift
+++ YuklemeTakip/Modules/ShipmentDetail/ShipmentDetailViewController.swift
@@ -17,10 +17,21 @@ class ShipmentDetailViewController: UIViewController {
     @IBOutlet weak var infoDetail6: InfoDetailView!
     @IBOutlet weak var infoDetail7: InfoDetailView!
     
+    @IBOutlet weak var documentView: UIView!
+    @IBOutlet weak var imgDocument: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.setStyle()
         self.setDetailsData()
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        imgDocument.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func setStyle(){
+        self.documentView.dropShadow()
     }
     
     func setDetailsData(){
@@ -31,6 +42,37 @@ class ShipmentDetailViewController: UIViewController {
         infoDetail5.setDatas("Yükleme Saati", "14:30")
         infoDetail6.setDatas("Hacim", "67 m3")
         infoDetail7.setDatas("Çıkış Gümrük", "Kapıkule")
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        let imgDocument = tapGestureRecognizer.view as! UIImageView
+        let newImageView = UIImageView(image: imgDocument.image)
+        newImageView.frame = UIScreen.main.bounds
+        newImageView.backgroundColor = .black
+        newImageView.contentMode = .scaleAspectFit
+        newImageView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage(sender:)))
+        newImageView.addGestureRecognizer(tap)
+        self.view.addSubview(newImageView)
+    }
+//
+//    @IBAction func imageTapped(sender: UITapGestureRecognizer) {
+//        let imgDocument = sender.view as! UIImageView
+//        let newImageView = UIImageView(image: imgDocument.image)
+//        newImageView.frame = UIScreen.main.bounds
+//        newImageView.backgroundColor = .black
+//        newImageView.contentMode = .scaleAspectFit
+//        newImageView.isUserInteractionEnabled = true
+//        let tap = UITapGestureRecognizer(target: self, action: Selector(("dismissFullscreenImage:")))
+//        newImageView.addGestureRecognizer(tap)
+//        self.view.addSubview(newImageView)
+//        self.navigationController?.isNavigationBarHidden = true
+//        self.tabBarController?.tabBar.isHidden = true
+//    }
+
+    @objc func dismissFullscreenImage(sender: UITapGestureRecognizer) {
+        sender.view?.removeFromSuperview()
     }
    
 
